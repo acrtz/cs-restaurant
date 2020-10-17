e6e1240ee1f52294a400130b2faf707b8de685e2
armandrtz <armandrtz@gmail.com>
2020-10-16 17:13:38 -0700

    make filter response/dynamic so it works with small screens

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,15 +1,35 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import RestaurantTable from "../RestaurantTable/RestaurantTable";
 import Filter from "../Filter/Filter";
 import TextSearch from "../TextSearch/TextSearch";
 import Pagination from "../Pagination/Pagination";
+import FilterIcon from "../../icons/filter";
+import CloseIcon from "../../icons/close";
 import { LayoutProps } from "../../types";
 import "./Layout.css";
 
 const Layout: React.FC<LayoutProps> = (props) => {
+  const [open, setOpen] = useState<boolean>(false);
+
   return (
-    <div id="layout">
-      <div id="left-panel">
+    <div
+      id="layout"
+      onClick={() => {
+        open && setOpen(false);
+      }}
+    >
+      <div
+        id="left-panel"
+        className={`${open ? "open" : ""}-filter`}
+        onClick={(e) => e.stopPropagation()}
+      >
+        <button
+          id="open-close-filter"
+          className={`${open ? "open" : ""}-filter`}
+          onClick={() => setOpen(!open)}
+        >
+          {open ? <CloseIcon /> : <FilterIcon />}
+        </button>
         <Filter
           filterGroups={props.filterGroups}
           filter={props.filter}
