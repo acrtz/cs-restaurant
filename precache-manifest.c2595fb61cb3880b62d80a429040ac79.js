cd10601450e81ba1219a3aad469f0427b33cae2f
armandrtz <armandrtz@gmail.com>
2020-10-18 22:03:06 -0700

    Updates

---
+++ precache-manifest.c2595fb61cb3880b62d80a429040ac79.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9478f6ac9f03cb13bdf3e0a9e85cdd84",
+    "url": "/cs-restaurants/index.html"
+  },
+  {
+    "revision": "3092fc952989eb026ab5",
+    "url": "/cs-restaurants/static/css/main.73ab004f.chunk.css"
+  },
+  {
+    "revision": "dd805e6edc50693f8695",
+    "url": "/cs-restaurants/static/js/2.c9cd99de.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/cs-restaurants/static/js/2.c9cd99de.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3092fc952989eb026ab5",
+    "url": "/cs-restaurants/static/js/main.6e01ee63.chunk.js"
+  },
+  {
+    "revision": "24b5c049314553345fa4",
+    "url": "/cs-restaurants/static/js/runtime-main.e251eab2.js"
+  }
+]);
